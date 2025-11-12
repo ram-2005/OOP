f61f155da493049a737f6f9b5485066726473781
ram-2005 <hanuram2005@gmail.com>
2025-11-12 12:13:05 +0530

    Added some comments to program4

--- Day-1/program4.java
+++ Day-1/program4.java
@@ -9,13 +9,14 @@ public class program4
     //getting the number from the user
     System.out.println("Enter a number :");
     int num = sc.nextInt();
-
+    
+    //if num is greater than 0 then its postive
     if(num > 0)
     {
       System.out.println("The entered number is positive");
-    } else if(num < 0) {
+    } else if(num < 0) { //if num is lesser than 0 then negative
       System.out.println("The entered number is negative");
-    } else {
+    } else { //if the num is equal to 0 then its O(ferrai meme reference)
       System.out.println("The entered number is Zero");
     }
 
